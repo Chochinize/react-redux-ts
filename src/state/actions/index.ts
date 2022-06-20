508984f9ce86927775585da87fc32150f886ef4a
Martin Aleksiev <chochinize@gmail.com>
2022-06-20 11:29:22 +0200

    stage all changes

---
+++ src/state/actions/index.ts
@@ -0,0 +1,13 @@
+import { ActionType} from '../action-type/index'
+interface DepositAction {
+    type:ActionType.DEPOSIT,
+    payload:number
+}
+interface WithdrawAction {
+    type:ActionType.WITHDRAW,
+    payload:number
+}
+interface BankruptAction {
+    type:ActionType.BANKRUPT
+}
+export type Action = DepositAction | WithdrawAction | BankruptAction
